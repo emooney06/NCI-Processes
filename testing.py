888ba833c1eff225d264fe9072944bba34b08791
Ethan Mooney <mooney.ethan@gmail.com>
2020-03-31 19:15:16 -0600

    added c19 update

--- testing.py
+++ testing.py
@@ -1,31 +1,63 @@
-import pandas as pd
-from pathlib import Path
-from functools import reduce
+from selenium import webdriver
+from time import sleep
+import re
+from datetime import datetime
+import smtplib
 
-pd.option_context('display.max_rows', None, 'display.max_columns', None)  # more options can be specified also
-pd.options.display.max_colwidth = 199
-pd.options.display.max_columns = 1000
-pd.options.display.width = 1000
-pd.options.display.precision = 2  # set as needed
 
-tc_path = Path('C:/Users/ejmooney/Desktop/testData/tc_users.xlsx')
-hr_path = Path('C:/Users/ejmooney/Desktop/testData/hr_report.xlsx')
-alias_path = Path('C:/Users/ejmooney/Desktop/testData/alias_report.xlsx')
+options = webdriver.ChromeOptions()
 
-hr = pd.read_excel(hr_path)
-tc = pd.read_excel(tc_path)
-alias = pd.read_excel(alias_path)
+driver_path = 'C:/Users/ejmooney/plugins/chromedriver'
+driver = webdriver.Chrome(executable_path=driver_path)
 
-alias['USERNAME'] = alias['USERNAME'].str.lower()
+class Coronavirus():
+    def __init__(self):
+        self.driver = webdriver.Chrome(executable_path=driver_path)
 
-hr = hr.rename(columns={'EE ID': 'ALIAS'})
+    def get_data(self):
+        self.driver.get('https://www.worldometers.info/coronavirus/')
+        table = self.driver.find_element_by_xpath("//*[@id='main_table_countries_today']/tbody[1]")
+        country_element = table.find_element_by_xpath("//td[contains(., 'USA')]")
+        row = country_element.find_element_by_xpath('./..')
+        data = row.text.split(' ')
+        total_cases = data[1]
+        new_cases = data[2]
+        total_deaths = data[3]
+        new_deaths = data[4]
+        active_cases = data[6]
+        total_recovered = data[5]
+        serious_critical = data[7]
 
-combinedData = reduce(lambda x,y: pd.merge(x,y, on='ALIAS', how='left'), [hr, alias])
+        send_mail(country_element.text, total_cases, new_cases, total_deaths, new_deaths, active_cases, total_recovered, serious_critical)
 
-tc = tc.rename(columns={'username':'USERNAME'})
+        self.driver.close()
+        self.driver.quit()
 
-combinedData = reduce(lambda x,y: pd.merge(x,y, on='USERNAME', how='left'), [combinedData, tc])
 
-write_path = Path('C:/Users/ejmooney/Desktop/testData/combined_tc_hr_alias.xlsx')
+def send_mail(country_element, total_cases, new_cases, total_deaths, new_deaths, active_cases,
+                total_recovered, serious_critical):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login('mooney.ethan@gmail.com', 'Arsenal#12020')
+    subject = 'Coronavirus stats today'
+    body = 'Today in ' + country_element + '\
+    \nThere is new data on coronavirus:\
+    \nTotal cases: ' + total_cases + '\
+    \nNew Cases: ' + new_cases + '\
+    \nTotal Deaths: ' + total_deaths + '\
+    \nNew Deaths: ' + new_deaths + '\
+    \nActive cases: ' + active_cases + '\
+    \nTotal recovered: ' + total_recovered + '\
+    \nSerious, critical cases: ' + serious_critical + '\
+    \n Check the link:  https://www.worldometers.info/coronavirus/'
 
-combinedData.to_excel(write_path)
+    msg = f'Subject: {subject}\n\n{body}'
+    server.sendmail('Coronavirus', 'mooney.ethan@gmail.com', msg)
+
+    print ("email sent!")
+    server.quit()
+
+bot = Coronavirus()
+bot.get_data()
