67f28d5c6441fd166edc0d9b88d319cb907afdfb
Ethan Mooney <mooney.ethan@gmail.com>
2020-05-08 14:09:12 -0600

    minor filter modification to covid_screen module

--- covid_screen.py
+++ covid_screen.py
@@ -5,13 +5,13 @@ from functools import reduce
 
 max_pd_display()
 
-data_path = Path('//uh-nas/Groupshare3/ClinicalAdvisoryTeam/data_folders/8940 - covid-screen')
-file_name = 'covid_screen.xlsx'
+data_path = Path('//uh-nas/Groupshare3/ClinicalAdvisoryTeam/data_folders/8940_covid_screen')
+file_name = '2020-05_covid_screen.xlsx'
 
 df = pd.read_excel(data_path / file_name)
 
 filter_by = ['P ICN-3 (IN3P)', 'P ICN-4 (IN4P)', 'P NBICU (NBIP)', 'P NB Nursery (NBNP)', 
-             'P Admit Prep (APIP)']
+             'P Admit Prep (APIP)', 'P MCICU (MCIP)', 'P OB Spec Care (HRMP)', 'P TSICU (TSIP)']
 
 
 df = df[~df.location.isin(filter_by)]
@@ -29,4 +29,4 @@ ma_df = ma_df.rename(columns={'cerner_unit_name': 'location'})
 
 concern_df = reduce(lambda x,y: pd.merge(x,y, on='location', how='left'), [pos_scrn_not_neg_test_df, ma_df])
 
-pos_scrn_not_neg_test_df.to_csv(data_path / 'test_out.csv')
+pos_scrn_not_neg_test_df.to_excel(data_path / 'test_out.xlsx')
