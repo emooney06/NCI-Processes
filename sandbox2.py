93c76ab7ec763135c7d9166419cfdf675df9c2c7
Ethan Mooney <mooney.ethan@gmail.com>
2020-05-18 15:44:41 -0600

    fixed consolidation problems

--- sandbox2.py
+++ sandbox2.py
@@ -1,83 +1,29 @@
-import pandas as pd
-import os
-from functools import reduce
-import string
-import numpy as np
-import collections
-import re
-from datetime import datetime
-from pathlib import Path
-from my_functions import max_pd_display, check_answer, make_string_cost_center, add_columns_for_reporting, double_check
-from my_classes import FileDateVars
-from my_variables import master_alias, mmm_dict
-import win32com.client
-import time 
+import smtplib, ssl
 
-max_pd_display()
 
-#define the file paths and file names
-rule_check_path = Path('//uh-nas/Groupshare3/ClinicalAdvisoryTeam/data_folders/rule_check_folder')
-rule_ck_out_file_name = 'rule_check_timestamp.csv'
-rule_ck_in_file_name = 'timestamp_from_message.csv'
+This message is sent from Python."""
+
+#define the file paths and file names0
+data_path = Path('//uh-nas/Groupshare3/ClinicalAdvisoryTeam/data_folders/8940_covid_screen')
+archive_path = Path('//uh-nas/Groupshare3/ClinicalAdvisoryTeam/data_folders/8940_covid_screen/8940_archive')
+file_name = 'covid_screen.xlsx'
 
 while True:
     try:
-        #generate a timestamp string
-        timestr = time.strftime("%Y%m%d-%H%M")
-        #open the file to write the timestamp to
-        timestamp = open(rule_check_path / rule_ck_out_file_name, 'r+')
-        #write the timestamp
-        timestamp.write(timestr)
-        #close the timestamp file
-        timestamp.close()
-        #initialize the outlook mail item using the local account
-        olMailItem = 0x0
-        #create an object to interface with Outlook
-        obj = win32com.client.Dispatch("Outlook.Application")
-        #create the new mail object
-        newMail = obj.CreateItem(olMailItem)
-        #generate the components
-        newMail.Subject = '#rule_check_message'
-        newMail.To = 'ejmooney@salud.unm.edu'
-        newMail.body = '''This is your automated rule-checking message.  This message is only intended to ensure Outlook is running and rule-checking is operational in general.  The 
-successful completion of this rule_check does not necessarily mean that ALL rules are operational. '''
-        #add the attachment to the message
-        newMail.Attachments.Add('//uh-nas/Groupshare3/ClinicalAdvisoryTeam/data_folders/rule_check_folder/rule_check_timestamp.csv')
-        #send the message
-        newMail.Send()
-        #sleep for 1 minute to allow the message to be received and the rule to excecute (delay of up to one minute has been observed)
-        time.sleep(65)
-        #read the file that has been saved by outlook rule "Rule Check"
-        from_msg = pd.read_csv(rule_check_path / rule_ck_in_file_name )
-        #recover the timestamp from the file saved by the rule
-        timestamp = from_msg.columns.values[0]
-        #convert the string timestamp to a datetime value
-        timestamp = datetime.strptime(timestamp, '%Y%m%d-%H%M')
-        #generate a timestamp for now
-        nowstamp = time.strftime('%Y%m%d-%H%M')
-        #convert the now timestamp to a datatime value
-        nowstamp = datetime.strptime(nowstamp, '%Y%m%d-%H%M')
-        #calculate the difference in the timestamp sent through the rule process and the now timestamp
-        diff = ((nowstamp - timestamp).total_seconds())
-        # if the difference between the timestamps is greater than 120 seconds, there is likely a problem with the Outlook rule; so send an email
-        if diff > 120:
-            alertMail = obj.CreateItem(olMailItem)
-            alertMail.Subject = 'Problem with Outlook Rules'
-            alertMail.To = 'ejmooney@salud.unm.edu'
-            alertMail.body = '''Your rule check process has detected a problem with an outlook rule.  Please check your "server machine" to ensure Outlook is running correctly.'''
-            alertMail.Send()
-            print(str(nowstamp) + ' try statment executed and detected a problem; message sent and sleeping for 1 hour')
-            time.sleep(3600)
-        else:
-            #if the difference between the timestamps is < 120 seconds, the rule appears to be in place so no action/alert needs to be made
-            print(str(nowstamp) + ' try statement executed without any issue, sleeping for 1 hour')
-            time.sleep(3600)
+    #create a timestamp for the archive file name
+        timestr = time.strftime("%Y%m%d-%H%M_")
+        #add the time stamp to the file name to create the archive file name string
+        archive_file =  timestr + file_name 
+        print('attempting to read the file')
+        #read the file from the PI report
+        df = pd.read_excel(data_path / file_name)
+        #save the dataframe as an archive
+        df.to_excel(archive_path / archive_file, index=False)
+        # drop duplicates
+        df = df.drop_duplicates()
     except:
-        #if for some reason an exception is encountered, handle it with an email alert and retry the process in an hour
-        alertMail2 = obj.CreateItem(olMailItem)
-        alertMail2.Subject = 'Problem with Outlook Rules'
-        alertMail2.To = 'ejmooney@salud.unm.edu'
-        alertMail2.body = '''Your rule check process has triggered an except statement.  Please check your "server machine" to ensure Outlook and the python script is running correctly.'''
-        alertMail2.Send()
-        print(str(nowstamp) + ' exception triggered; sleeping for 1 hour')
-        time.sleep(3600)
+        print('executing the except statement')
+        #sleep for 24 hours
+        time.sleep(20)
+    print("now i'm doing the stuff")
+    time.sleep(30)
